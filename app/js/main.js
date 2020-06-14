c0001459d3a3249a59dde3e527bc5c5773b6a1bd
Ivan Dimitrov <ivan.dimitrov293@gmail.com>
2020-06-15 01:17:31 +0300

    [Fix] Fixed tab identation

--- app/js/main.js
+++ app/js/main.js
@@ -1,22 +1,22 @@
 const toggleDrawer = () => {
-    const drawerClassList = event.target.classList
-    const navClassList = document.querySelectorAll('.header-navbar')[0].classList
+	const drawerClassList = event.target.classList
+	const navClassList = document.querySelectorAll('.header-navbar')[0].classList
 
-    drawerClassList.toggle('opened')
-    navClassList.toggle('opened')
-    document.body.classList.toggle('noscroll')
+	drawerClassList.toggle('opened')
+	navClassList.toggle('opened')
+	document.body.classList.toggle('noscroll')
 }
 
 const toggleDropdown = () => {
-    event.stopPropagation()
-    if(event.target.querySelector('.dropdown-list')){
-        const dropDownClassList = event.target.querySelector('.dropdown-list').classList
-        
-        dropDownClassList.toggle('opened')
-    } 
+	event.stopPropagation()
+	if(event.target.querySelector('.dropdown-list')){
+		const dropDownClassList = event.target.querySelector('.dropdown-list').classList
+		
+		dropDownClassList.toggle('opened')
+	} 
 
-    else if (event.target.classList.contains('dropdown-button')){
-        event.target.nextElementSibling.classList.toggle('opened')
-    }
+	else if (event.target.classList.contains('dropdown-button')){
+		event.target.nextElementSibling.classList.toggle('opened')
+	}
 
 }
